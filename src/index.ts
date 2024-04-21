3b5eae457daf1b4e08bb6a06fc2411fd79c1b4b6
Aneesha Rama <aneesharama@gmail.com>
2024-04-21 17:09:06 +0530

    simple express server

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import {json} from 'body-parser'
+const app = express();
+
+const port=3000;
+
+app.use(json());
+
+app.get("/", (req, res)=>{
+    res.json({message: "Hello from the server"});
+})
+
+app.listen(port, ()=>{
+    console.log(`Server is running on port: ${port}`);
+})
