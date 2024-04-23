0600de54d600733d76154c15d7d078ee3147245d
Aneesha Rama <aneesharama@gmail.com>
2024-04-23 20:18:15 +0530

    signing commit

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-import express from 'express';
-import {json} from 'body-parser'
+import express from "express";
+import { json } from "body-parser";
 const app = express();
 
-const port=3000;
+const port = 3000;
 
 app.use(json());
 
-app.get("/", (req, res)=>{
-    res.json({message: "Hello from the server. V3!!"});
-})
+app.get("/", (req, res) => {
+  res.json({ message: "Hello from the server. V3!!!" });
+});
 
-app.listen(port, ()=>{
-    console.log(`Server is running on port: ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
